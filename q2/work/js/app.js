25b9f301330a8d3c7b4717e72b3eecd7c40b0aa0
yuki8703 <arumaji0087@gmail.com>
2022-07-16 16:28:33 +0900

    モーダルウィンドウ実装済み

--- q2/work/js/app.js
+++ q2/work/js/app.js
@@ -1,10 +1,10 @@
 $(function() {
     $('.modal_open_button').on('click', function() {
-        $('.modal_win').fadeIn();
+        $('.modal_win').fadeIn(); //OPEN押したらモーダルが開く
         return false;
     });
     $('.modal_close_button').on('click', function() {
-        $('.modal_win').fadeOut();
+        $('.modal_win').fadeOut(); //×ボタン押したら閉じる
         return false;
     });
 });
